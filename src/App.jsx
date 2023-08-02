ed44fb2149c23eb4922d4f592a5ea104ed1458f9
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-08-02 17:02:57 -0600

    feat(components): 💄 Add portfolio headline and darkmode.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
-import './App.css'
+import { useState, useEffect } from 'react'
+import HeroSection from './components/HeroSection'
 
 function App () {
+  const [theme, setTheme] = useState(null)
+
+  useEffect(() => {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      setTheme('dark')
+    } else {
+      setTheme('light')
+    }
+  }, [])
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  }, [theme])
+
+  function handleThemeSwitch () {
+    setTheme(theme === 'dark' ? 'light' : 'dark')
+  }
+
   return (
     <>
-      <h2>Hello World!</h2>
+      <button
+        type='button'
+        onClick={handleThemeSwitch}
+        className='fixed z-10 right-2 top-2 bg-indigo-500 text-lg p-1 rounded-md'
+      >
+        {theme === 'dark' ? '🌚' : '🌞'}
+      </button>
+      <div className='font-inter bg:white dark:bg-slate-900'>
+        <HeroSection />
+      </div>
     </>
   )
 }
