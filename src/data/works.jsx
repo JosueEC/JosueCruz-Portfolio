7585b72a586d1c1b99ddbc510a61929a1630c72b
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-08-08 16:36:06 -0600

    feat(src): ✨ Add CommitMaker project to the projects sections

--- src/data/works.jsx
+++ src/data/works.jsx
@@ -1,4 +1,5 @@
 import profinderData from '../data/profinder'
+import commitMakerData from '../data/commitMakerData'
 
 export default [
   {
@@ -15,6 +16,6 @@ export default [
     description: 'CommitMaker es una aplicacion que te ayuda en la creacion de commits basados en el estandar ConventionalCommits, la idea esta pensada para que este estandar de commits sea mas facil de crear y entender por personas que recien estan aprendiendo a utilizarlos.',
     linkDeploy: 'https://commitmaker.vercel.app/',
     linkRepository: 'https://github.com/JosueEC/CommitsMaker',
-    featuresData: profinderData
+    featuresData: commitMakerData
   }
 ]
