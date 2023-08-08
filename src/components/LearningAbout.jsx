e5e25aa3053db3ecdd3a427db30d936a68ee2aba
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-08-08 10:41:08 -0600

    feat(components): 💄 Add elements to the LearningAbout section.

--- src/components/LearningAbout.jsx
+++ src/components/LearningAbout.jsx
@@ -5,7 +5,7 @@ import learning from '../data/learning'
 export default function LearningAbout () {
   return (
     <div>
-      <SectionTitle>Learning About</SectionTitle>
+      <SectionTitle>Begginer In</SectionTitle>
       <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-5'>
         {
           learning.map(({ title, icon }) => {
