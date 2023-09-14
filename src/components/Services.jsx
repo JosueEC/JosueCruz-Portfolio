8d391035e00708cb2a249fed25c9f634cc19152f
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-14 13:26:59 -0600

    feat(components): 🔨 Modify the level skills and add new skills to the service section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -5,7 +5,7 @@ import services from '../data/services'
 export default function Services () {
   return (
     <div className='py-12'>
-      <SectionTitle>Proeficiente en</SectionTitle>
+      <SectionTitle>Cuento con dominio avanzado en:</SectionTitle>
       <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-5'>
         {services.map(({ title, icon }) => {
           return (
--- src/data/learning.jsx
+++ src/data/learning.jsx
@@ -2,29 +2,17 @@ import { FaJava } from 'react-icons/fa'
 import {
   SiHibernate,
   SiSpringboot,
-  SiTailwindcss,
-  SiTypescript,
-  SiDocker,
-  SiMongodb,
-  SiMongoose
+  SiDocker
 } from 'react-icons/si'
 
 export default [
-  {
-    title: 'TypeScript',
-    icon: <SiTypescript className='w-full h-full' />
-  },
   {
     title: 'Docker',
     icon: <SiDocker className='w-full h-full' />
   },
   {
-    title: 'MongoDb',
-    icon: <SiMongodb className='w-full h-full' />
-  },
-  {
-    title: 'Mongoose',
-    icon: <SiMongoose className='w-full h-full' />
+    title: 'Docker-Compose',
+    icon: <SiDocker className='w-full h-full' />
   },
   {
     title: 'Java',
@@ -37,9 +25,5 @@ export default [
   {
     title: 'Hibernate',
     icon: <SiHibernate className='w-full h-full' />
-  },
-  {
-    title: 'TailwindCSS',
-    icon: <SiTailwindcss className='w-full h-full' />
   }
 ]
