aa7d5b9151a99d547f3ef7676ff72a7902c7074e
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-14 13:41:39 -0600

    fix(assets): 🐛 Fix problem with the CV file

--- src/singleComponents/Buttons/ButtonDownloadFile.jsx
+++ src/singleComponents/Buttons/ButtonDownloadFile.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import CV from '../../assets/Files/Josue Cruz - Desarrollador Web.pdf'
+import CV from '../../assets/files/Josue Cruz - Desarrollador Web.pdf'
 
 export default function ButtonDownloadFile ({ text }) {
   return (
