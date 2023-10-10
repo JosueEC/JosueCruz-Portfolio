6a2e8f3373a7dce8f62e6704c0a6affd26be7aab
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-10-09 22:16:56 -0600

    styles(ButtonLink.jsx): 🎨 Change the component background color and replace it by a 'Typography' component

--- src/singleComponents/Buttons/ButtonDownloadFile.jsx
+++ src/singleComponents/Buttons/ButtonDownloadFile.jsx
@@ -1,10 +1,16 @@
 /* eslint-disable react/prop-types */
+import { Button } from '@material-tailwind/react'
 import CV from '../../assets/documents/Josue Cruz - Desarrollador Web.pdf'
 
 export default function ButtonDownloadFile ({ text }) {
   return (
-    <a href={CV} target='_blank' rel='noopener noreferrer' download='Josue Cruz - Desarrollador Web.pdf' className='inline-flex items-center justify-center text-sm md:text-xl font-semibold text-white duration-200 bg-indigo-600 px-4 py-2.5 rounded-md hover:bg-indigo-700 focus:outline-none focus-visible:outline-gray-600'>
-      {text}
-    </a>
+    <Button
+      variant='gradient'
+      color='cyan'
+    >
+      <a href={CV} target='_blank' rel='noopener noreferrer' download='Josue Cruz - Desarrollador Web.pdf'>
+        {text}
+      </a>
+    </Button>
   )
 }
--- src/singleComponents/Buttons/ButtonLink.jsx
+++ src/singleComponents/Buttons/ButtonLink.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 export default function ButtonLink ({ icon, text, linkPage }) {
   return (
-    <a href={linkPage} target='_blank' rel='noreferrer' className='inline-flex items-center justify-center text-sm md:text-xl font-semibold text-white duration-200 bg-indigo-600 px-4 py-2.5 rounded-md hover:bg-indigo-700 focus:outline-none focus-visible:outline-gray-600 gap-3'>
+    <a href={linkPage} target='_blank' rel='noreferrer' className='inline-flex items-center justify-center text-sm md:text-xl font-semibold text-gray-50 duration-200 bg-cyan-500 px-4 py-2.5 rounded-md hover:bg-cyan-700 focus:outline-none focus-visible:outline-gray-600 gap-3'>
       {icon} {text}
     </a>
   )
