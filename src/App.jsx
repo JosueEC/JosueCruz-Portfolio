512fc70332d7aec2ff47f0d376c874321675339a
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-10-09 22:26:57 -0600

    feat(Services.jsx): 🔨 Add a gradient element to the section

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import { useState, useEffect } from 'react'
 import Hero from './components/Hero'
 import Services from './components/Services'
 import LearningAbout from './components/LearningAbout'
-import Works from './components/Works'
 import About from './components/About'
 import Footer from './components/Footer'
+import NavListMenu from './components/NavListMenu/NavListMenu'
+import Profinder from './components/Projects/Profinder/Profinder'
 
 function App () {
   const [theme, setTheme] = useState(null)
@@ -25,20 +26,10 @@ function App () {
     }
   }, [theme])
 
-  // function handleThemeSwitch () {
-  //   setTheme(theme === 'dark' ? 'light' : 'dark')
-  // }
-
   return (
     <>
-      {/* <button
-        type='button'
-        onClick={handleThemeSwitch}
-        className='fixed z-10 right-2 top-2 bg-indigo-500 text-lg p-1 rounded-md'
-      >
-        {theme === 'dark' ? '🌚' : '🌞'}
-      </button> */}
-      <div className='font-inter bg-gradient-to-br from-slate-900 to-indigo-800'>
+      <div className='font-inter bg-gradient-to-bl from-neutral-900 to-slate-900'>
+        <NavListMenu />
         <div className='max-w-5xl mx-auto w-11/12'>
           <Hero />
           <div className='md:h-screen'>
@@ -46,7 +37,7 @@ function App () {
             <LearningAbout />
           </div>
         </div>
-        <Works />
+        <Profinder />
         <div className='max-w-5xl mx-auto w-11/12'>
           <About />
         </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,31 @@
 import { BsGithub, BsLinkedin } from 'react-icons/bs'
 import ButtonAction from '../singleComponents/Buttons/ButtonAction'
 import ButtonDownloadFile from '../singleComponents/Buttons/ButtonDownloadFile'
+import { Typography } from '@material-tailwind/react'
 
 export default function Hero () {
   return (
-    <section id='hero' className='md:h-screen flex items-center'>
+    <section id='hero' className='relative md:h-screen flex items-center'>
+      <div aria-hidden='true' className='absolute w-[200px] h-[200px] lg:w-[300px] lg:h-[300px] rounded-full bg-gradient-to-b from-blue-600 to-purple-600 blur-3xl opacity-50 top-[10%] left-[20%] md:left-[30%] md:top-[15%] lg:top-[20%] lg:left-[37%]' />
       <div className='items-center px-8 py-12 mx-auto max-w-7xl lg:px-16 md:px-12 lg:py-24'>
         <div className='justify-center w-full text-center lg:p-2 max-auto'>
           <div className='justify-center w-full mx-auto'>
-            <h1 className='text-6xl md:text-8xl mb-1 md:mb-3 text-indigo-600 font-semibold dark:text-indigo-500'>
-              Hola, soy Josue
-            </h1>
-            <p className='max-w-xl mx-auto mt-4 text-lg md:text-3xl tracking-tight text-gray-600 dark:text-gray-300'>
-              Desarrollador Web
-            </p>
+            <Typography
+              as='h2'
+              variant='h2'
+              color='cyan'
+              className='sm:text-6xl md:text-8xl'
+              textGradient
+            >
+              Josue Cruz
+            </Typography>
+            <Typography
+              as='h1'
+              color='white'
+              className='sm:text-2xl md:text-4xl'
+            >
+              Desarrollador Web Backend
+            </Typography>
           </div>
           <div className='flex flex-row items-center justify-center max-w-xl gap-3 mx-auto mt-10 lg:flex-row'>
             <ButtonAction text='Ver proyectos' />
@@ -23,13 +35,13 @@ export default function Hero () {
 
         <div className='flex justify-center gap-6 mt-6'>
           <a className='p-1 -m-1 group' href='https://github.com/JosueEC' target='_blank' rel='noreferrer'>
-            <BsGithub class='w-6 h-6 transition text-indigo-600
-            dark:text-white hover:fill-indigo-500 md hydrated'
+            <BsGithub className='w-6 h-6 transition text-indigo-600
+            dark:text-white hover:fill-cyan-400 md hydrated'
             />
           </a>
           <a className='p-1 -m-1 group' aria-label='Follow on LinkedIn' href='https://www.linkedin.com/in/josue-evangelista-cruz' target='_blank' rel='noreferrer'>
-            <BsLinkedin class='w-6 h-6 transition text-2xl text-indigo-600
-            dark:text-white hover:fill-indigo-500 md hydrated'
+            <BsLinkedin className='w-6 h-6 transition text-2xl text-indigo-600
+            dark:text-white hover:fill-cyan-400 md hydrated'
             />
           </a>
         </div>
