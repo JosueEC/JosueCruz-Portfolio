f64893e1845458570682a465efe5238c4ef773bf
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-10-09 22:11:48 -0600

    feat(tailwind.config.js): 🔨 Add 'slate' and 'neutral' colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,23 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{jsx, js}'],
+const withMT = require('@material-tailwind/react/utils/withMT')
+
+module.exports = withMT({
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
         inter: ['inter', 'serif']
+      },
+      colors: {
+        slate: {
+          900: 'rgb(15 23 42)'
+        },
+        neutral: {
+          900: 'rgb(23 23 23)'
+        }
       }
     }
   },
   plugins: []
-}
+})
