24cb6c60a4728dae43a298ce8463d6dc4a483bd4
JosueEC <josueev.cruz@gmail.com>
2025-04-28 20:25:15 -0600

    [CHORE](Services.jsx): Update the services list

--- src/data/services.jsx
+++ src/data/services.jsx
@@ -1,104 +1,78 @@
-import {
-  FaNodeJs,
-  FaReact,
-  FaHtml5,
-  FaCss3Alt,
-  FaGitAlt,
-  FaJava
-} from 'react-icons/fa'
+import { FaNodeJs, FaReact, FaPython } from "react-icons/fa";
 import {
   SiExpress,
-  SiSequelize,
   SiRedux,
   SiJavascript,
   SiNestjs,
   SiTypescript,
-  SiMongoose,
   SiMongodb,
-  SiTailwindcss,
-  SiSpringboot,
-  SiHibernate
-} from 'react-icons/si'
-import {
-  BiLogoPostgresql
-} from 'react-icons/bi'
-import { VscTypeHierarchySub } from 'react-icons/vsc'
+  SiDocker,
+  SiGraphql,
+  SiRabbitmq,
+  SiGithubactions,
+} from "react-icons/si";
+import { BiLogoPostgresql } from "react-icons/bi";
+import { PiFileSqlFill } from "react-icons/pi";
 
 export default [
   {
-    title: 'Java',
-    icon: <FaJava className='w-full h-full' />
+    title: "NodeJS",
+    icon: <FaNodeJs className="w-full h-full" />,
   },
   {
-    title: 'Spring-boot',
-    icon: <SiSpringboot className='w-full h-full' />
+    title: "JavaScript",
+    icon: <SiJavascript className="w-full h-full" />,
   },
   {
-    title: 'Hibernate',
-    icon: <SiHibernate className='w-full h-full' />
+    title: "TypeScript",
+    icon: <SiTypescript className="w-full h-full" />,
   },
   {
-    title: 'NodeJS',
-    icon: <FaNodeJs className='w-full h-full' />
+    title: "SQL",
+    icon: <PiFileSqlFill className="w-full h-full" />,
   },
   {
-    title: 'JavaScript',
-    icon: <SiJavascript className='w-full h-full' />
+    title: "Python",
+    icon: <FaPython className="w-full h-full" />,
   },
   {
-    title: 'TypeScript',
-    icon: <SiTypescript className='w-full h-full' />
+    title: "Express",
+    icon: <SiExpress className="w-full h-full" />,
   },
   {
-    title: 'Express',
-    icon: <SiExpress className='w-full h-full' />
+    title: "NestJS",
+    icon: <SiNestjs className="w-full h-full" />,
   },
   {
-    title: 'NestJS',
-    icon: <SiNestjs className='w-full h-full' />
+    title: "GraphQL",
+    icon: <SiGraphql className="w-full h-full" />,
   },
   {
-    title: 'TypeORM',
-    icon: <VscTypeHierarchySub className='w-full h-full' />
+    title: "RabbitMQ",
+    icon: <SiRabbitmq className="w-full h-full" />,
   },
   {
-    title: 'Sequelize',
-    icon: <SiSequelize className='w-full h-full' />
+    title: "GitHub Actions",
+    icon: <SiGithubactions className="w-full h-full" />,
   },
   {
-    title: 'Mongoose',
-    icon: <SiMongoose className='w-full h-full' />
+    title: "PostgreSQL",
+    icon: <BiLogoPostgresql className="w-full h-full" />,
   },
   {
-    title: 'PostgreSQL',
-    icon: <BiLogoPostgresql className='w-full h-full' />
+    title: "MongoDB",
+    icon: <SiMongodb className="w-full h-full" />,
   },
   {
-    title: 'MongoDB',
-    icon: <SiMongodb className='w-full h-full' />
+    title: "ReactJS",
+    icon: <FaReact className="w-full h-full" />,
   },
   {
-    title: 'ReactJS',
-    icon: <FaReact className='w-full h-full' />
+    title: "Redux",
+    icon: <SiRedux className="w-full h-full" />,
   },
   {
-    title: 'TailwindCSS',
-    icon: <SiTailwindcss className='w-full h-full' />
+    title: "Docker",
+    icon: <SiDocker className="w-full h-full" />,
   },
-  {
-    title: 'Redux',
-    icon: <SiRedux className='w-full h-full' />
-  },
-  {
-    title: 'HTML',
-    icon: <FaHtml5 className='w-full h-full' />
-  },
-  {
-    title: 'CSS',
-    icon: <FaCss3Alt className='w-full h-full' />
-  },
-  {
-    title: 'Git',
-    icon: <FaGitAlt className='w-full h-full' />
-  }
-]
+];
